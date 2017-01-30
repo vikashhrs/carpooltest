8e70b88a3e22e62830cd994a6298cfa994159dce
Vikash Kumar Sharma <vikashhrs@gmail.com>
2017-01-30 23:30:13 +0530

    Views Added

---
+++ app/src/main/java/com/example/vikash/carpooltest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vikash.carpooltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
---
+++ app/src/main/java/com/example/vikash/carpooltest/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.vikash.carpooltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
